706f700dabfda6e777605f1121c6707224aea752
Ryan B Leach <rleach@ebsco.com>
2014-10-20 15:05:24 -0400

    try just updating the file ref

--- bumpdep.js
+++ bumpdep.js
@@ -1,66 +1,31 @@
 var fs = require('fs'),
 	_ = require('lodash'),
-	exec = require('child_process').exec,
 	when = require('when'),
-	findup = require('findup-sync'),
-	packageVersion;
+	findup = require('findup-sync');
 
-var formatRepo = function(pkgName, pkgRef) {
+var formatReference = function(pkgRef, pkgName, pkgVersion) {
 	if (/#|\/\//g.test(pkgRef)) {
-		return pkgRef.split('#')[0] + '#';
+		return pkgRef.split('#')[0] + '#' + pkgVersion;
 	} else {
-		return pkgName + '@';
+		return pkgVersion;
 	}
 };
 
-var updateReference = function(pkgFile, pkgName, pkgVersion, deferred) {
-	var thisPkg = require(pkgFile),
-		manager = /bower\.json$/.test(pkgFile) ? 'bower' : 'npm';/*,
-		resolver = deferred.resolve;
-
-	if (manager === 'npm') {
-		resolver = function() {
-			exec('npm shrinkwrap;', function(err, stdOut, stdErr) {
-				if (err || stdErr) {
-					throw(err || stdErr);
-				}
-				console.log(stdOut);
-				deferred.resolve();
-			});
-		};
-	}*/
+var updateReference = function(pkgFile, pkgName, pkgVersion) {
+	var thisPkg = require(pkgFile);
 
 	['dependencies', 'devDependencies'].forEach(function(key) {
 		var depsHash = thisPkg[key],
-			cmdData = {
-				flag: (key === 'dependencies' ? '--save' : '--save-dev'),
-				repo: pkgName,
-				tag: pkgVersion,
-				mngr: manager
-			},
-			cmd = '<%=mngr%> install <%=flag%> <%=repo%><%=tag%>';
-
-		if (manager === 'bower') {
-			cmdData.flag = '--allow-root --config.interactive=false ' + cmdData.flag;
-		} else {
-			cmdData.flag = '--save-exact ' + cmdData.flag;
-		}
+			pkgRef;
 
 		if (depsHash && depsHash[pkgName]) {
-			cmdData.repo = formatRepo(pkgName, depsHash[pkgName]);
-			console.log('Updating dependency using command:');
-			console.log(_.template(cmd, cmdData));
-			exec(_.template(cmd, cmdData), function(err, stdOut, stdErr) {
-				if (err || stdErr) {
-					console.error(err || stdErr);
-					deferred.reject(err);
-				}
-				console.log(stdOut);
-				deferred.resolve();
-			});
+			pkgRef = formatReference(depsHash[pkgName], pkgName, pkgVersion);
+			console.log('Updating dependency reference from: %s to %s', depsHash[pkgName], pkgRef);
+			depsHash[pkgName] = pkgRef;
 		}
 	});
-
+	console.log(thisPkg);
+	// fs.writeFileSync(pkgFile, JSON.stringify(thisPkg, null, '  '));
 };
 
 module.exports = function(name, tag) {
@@ -88,6 +53,59 @@ module.exports = function(name, tag) {
 	when.all(mngrProms).done(function() {
 		console.log('Finished updating dependency: %s@%s', name, tag);
 	}, function(err) {
-		throw(err);
+		console.error(err);
 	});
 };
+
+
+/*
+var updateReference = function(pkgFile, pkgName, pkgVersion, deferred) {
+	var thisPkg = require(pkgFile),
+		manager = /bower\.json$/.test(pkgFile) ? 'bower' : 'npm';,
+		resolver = deferred.resolve;
+
+	if (manager === 'npm') {
+		resolver = function() {
+			exec('npm shrinkwrap;', function(err, stdOut, stdErr) {
+				if (err || stdErr) {
+					throw(err || stdErr);
+				}
+				console.log(stdOut);
+				deferred.resolve();
+			});
+		};
+	}
+
+	['dependencies', 'devDependencies'].forEach(function(key) {
+		var depsHash = thisPkg[key],
+			cmdData = {
+				flag: (key === 'dependencies' ? '--save' : '--save-dev'),
+				repo: pkgName,
+				tag: pkgVersion,
+				mngr: manager
+			},
+			cmd = '<%=mngr%> install <%=flag%> <%=repo%><%=tag%>';
+
+		if (manager === 'bower') {
+			cmdData.flag = '--allow-root --config.interactive=false ' + cmdData.flag;
+		} else {
+			cmdData.flag = '--save-exact ' + cmdData.flag;
+		}
+
+		if (depsHash && depsHash[pkgName]) {
+			cmdData.repo = formatReference(depsHash[pkgName], pkgName);
+			console.log('Updating dependency using command:');
+			console.log(_.template(cmd, cmdData));
+			exec(_.template(cmd, cmdData), function(err, stdOut, stdErr) {
+				if (err || stdErr) {
+					console.error(err || stdErr);
+					deferred.reject(err || stdErr);
+				}
+				console.log(stdOut);
+				deferred.resolve();
+			});
+		}
+	});
+
+};
+*/
